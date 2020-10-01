c212467adff183d1d08483d32f5539a7e68e31c5
Rafa C <rafacab.dev@gmail.com>
2020-10-01 12:43:18 +0200

    Mejorado ejercicio bucle colores

--- html/u3/bucles/ej4colores.php
+++ html/u3/bucles/ej4colores.php
@@ -11,11 +11,11 @@
     *
     *   @author Rafa Caballero
     */
-$red = 0;
-$green = 0;
-$blue = 0;
+// $red = 0;
+// $green = 0;
+// $blue = 0;
 
-while ($red <= 255) {
+/* while ($red <= 255) {
     $red += 50;
     while ($green <= 255) {
         $green += 50;
@@ -28,7 +28,24 @@ while ($red <= 255) {
     }
     echo("<div style=\"background-color:rgb(" . $red . "," . $green . "," . $blue . ");height:60px;width:60px;display:inline-block;\"></div>");
     $green = 0;
+}*/
+
+echo("<table>");
+for ($red = 0; $red <= 255; $red += 50) {
+    for ($green = 0; $green <= 255; $green += 50) {
+        echo("<tr>");
+        
+        for ($blue = 0; $blue <= 255; $blue += 50) {
+            $hex = "#".dechex($red).dechex($green).dechex($blue);
+            $rgb = "rgb(".$red.",".$green.",".$blue.")";
+            echo("<td style=\"background-color:".$rgb."\">".$hex."</td>");
+        }
+
+        echo("</tr>");
+    }
 }
+
+echo("</table>");
 ?>
 </body>
 </html>

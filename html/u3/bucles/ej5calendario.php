30dc9e89d18fd08e8d50fac3ed65bf718a072104
Rafa C <rafacab.dev@gmail.com>
2020-10-02 10:50:07 +0200

    Añadido ejercicio calendario

--- html/u3/bucles/ej5calendario.php
+++ html/u3/bucles/ej5calendario.php
@@ -11,13 +11,15 @@
     *
     *   @author Rafa Caballero
     */
-$dia = 26; // Entero
+$dia = 2; // Entero
 $mes = 10; // Entero
 $anno = 2020; // Entero
 $dias_mes = 0; // Entero
 $nombre_mes = ""; // String
 $color = ""; // String
-$festivos =  array(12, 24);  // Array
+$festivosAutonomicos = array();
+$festivosNacionales = array();
+$festivosLocales = array();
 
 $bisiesto = false;
 
@@ -52,10 +54,12 @@ endif;
 switch ($mes) {
     case 1 :
         $nombre_mes = "enero";
+        $festivosNacionales = array(1, 6);
         break;
 
     case 2:
         $nombre_mes = "febrero";
+        $festivosAutonomicos = array(28);
         break;
     
     case 3:
@@ -64,10 +68,12 @@ switch ($mes) {
 
     case 4:
         $nombre_mes = "abril";
+        $festivosNacionales = array(10);
         break;
     
     case 5:
         $nombre_mes = "mayo";
+        $festivosNacionales = array(1);
         break;
 
     case 6:
@@ -80,46 +86,112 @@ switch ($mes) {
 
     case 8:
         $nombre_mes = "agosto";
+        $festivosNacionales = array(15);
         break;
 
     case 9:
         $nombre_mes = "septiembre";
+        $festivosLocales = array(8);
         break;
 
     case 10:
         $nombre_mes = "octubre";
+        $festivosNacionales = array(12);
+        $festivosLocales = array(24);
         break;
 
     case 11:
         $nombre_mes = "noviembre";
+        $festivosNacionales = array(1);
         break;
 
     case 12:
         $nombre_mes = "diciembre";
+        $festivosNacionales = array(6, 8, 25);
         break;
 
     default:
         $nombre_mes = "enero";
+        $festivosNacionales = array(1, 6);
         $mes = 1;
         break;
 }
 
-$j = 0;
+// Calculo que día de la semana es el primero
+$primerdia = date("w", mktime(0, 0, 0, $mes, 1, $anno));
+
+
+$j = 0; // Con esta variable controlo en que posición del calendario estoy
+        // para saber cuando debo saltar de fila en la tabla
+
 echo("<table border><tr><th colspan=\"7\">". $nombre_mes . " ". $anno . "</th></tr>");
+echo("<tr>
+        <th>L</th>
+        <th>M</th>
+        <th>X</th>
+        <th>J</th>
+        <th>V</th>
+        <th>S</th>
+        <th>D</th>
+    </tr>");
+
+// Si el primer día del mes cae en domingo $primerdia es 0, lo hago a parte para dejar 6 huecos y no 0
+if ($primerdia == 0) {
+    for ($a = 1; $a < 7; $a++) {
+        if ($j == 0) {
+            echo("<tr>");
+        }
+    
+        echo("<td></td>");
+    
+        // Si el número llega a 7 acabo la fila
+        if ($j >= 7) {
+            echo("</tr>");
+            $j = 0;
+        }
+        $j++;
+    }
+} else {
+    for ($a = 1; $a < $primerdia; $a++) { // Dejo tantos huecos como la posición del día de la semana
+        if ($j == 0) {
+            echo("<tr>");
+        }
+    
+        echo("<td></td>");
+    
+        // Si el número llega a 7 acabo la fila
+        if ($j >= 7) {
+            echo("</tr>");
+            $j = 0;
+        }
+        $j++;
+    }
+}
+
 for ($i = 1; $i <= $dias_mes; $i++) {
 
     // Color blanco por defecto
     $color = "white";
 
+    // Si $i es festivo nacional que el fondo sea rojo
+    // https://www.php.net/manual/es/function.in-array.php
+    if (in_array($i, $festivosNacionales)) {
+        $color = "red";
+    }
+
+    // Si es festivo autonómico naranja... (Andalucía)
+    if (in_array($i, $festivosAutonomicos)) {
+        $color = "orange";
+    }
+
+    // Si es festivo local azul... (Córdoba)
+    if (in_array($i, $festivosLocales)) {
+        $color = "blue";
+    }
+
     // Si $i es hoy que el fondo sea verde
     if ($i == $dia) {
         $color = "green";
-    }
-
-    // Si $i es festivo que el fondo sea rojo
-    // https://www.php.net/manual/es/function.in-array.php
-    if (in_array($i, $festivos)) {
-        $color = "red";
     }
 
     // Empiezo la fila
